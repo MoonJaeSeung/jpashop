ec13de793e8543127bbe6acdd374b1c0ebac3009
MoonJaeSeung <wotmdskfk32@naver.com>
2023-05-23 01:47:39 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        //엔티티 매니저는 쓰레드간에 공유 X (사용하고 버려야 한다)
+
+        EntityTransaction tx = em.getTransaction(); // jpa의 모든 데이터의 변경은 트랜잭션 안에서 실행
+        tx.begin();
+
+        try {
+
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
